9922817b765f685f2eb59850b178622e0097aa47
Evgeniy-github-acc <3374555@gmail.com>
2021-09-24 18:12:08 +0300

    devise v1

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,11 @@ Rails.application.routes.draw do
 
   root 'tests#index'
 
-  get :signup, to: 'users#new'
-  get :login, to: 'sessions#new'
-  get :logout, to: 'sessions#destroy'
-  
-  resources :users, only: :create
-  resources :sessions, only: :create
+  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout },
+                                   controllers: { sessions: 'users/sessions' }
 
-  resources :tests do
-    resources :questions, shallow: true, except: :index do
-      resources :answers, shallow: true, except: :index
-    end
 
+  resources :tests, only: :index do
     member do 
       post :start
     end
@@ -25,4 +18,12 @@ Rails.application.routes.draw do
     end
   end
 
+  namespace :admin do
+    resources :tests do
+      resources :questions, shallow: true, except: :index do
+        resources :answers, shallow: true, except: :index
+      end  
+    end
+
+  end
 end
