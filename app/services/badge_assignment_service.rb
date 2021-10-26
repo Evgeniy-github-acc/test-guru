69310f93ed7e46179bec0afb3d88e0bd344d813d
Evgeniy-github-acc <3374555@gmail.com>
2021-10-26 13:50:22 +0300

    badges 2

--- app/services/badge_assignment_service.rb
+++ app/services/badge_assignment_service.rb
@@ -26,14 +26,16 @@ class BadgeAssignmentService
 
   def complete_category_assign?(category_title)
     category_tests = Test.tests_by_category_title(category_title)
-    passed_tests = category_tests.user_succeed_tests(@user.id)
-    passed_tests.ids == category_tests.ids && @user.badges.where(rule: 'complete_category_assign?', rule_param: category_title).empty?
+    passed_tests = category_tests.user_succeed_tests(@user.id).ids.uniq
+   
+    passed_tests == category_tests.ids && @user.badges.where(rule: 'complete_category_assign?', rule_param: category_title).empty?
   end
 
   def complete_level_assign?(level)
     level_tests = Test.level_sort(@test_passage.test.level)
-    passed_tests = level_tests.user_succeed_tests(@user.id)
-    passed_tests == level_tests && @user.badges.where(rule: 'complete_level_assign?', rule_param: level).empty?
+    passed_tests = level_tests.user_succeed_tests(@user.id).ids.uniq
+
+    passed_tests == level_tests.ids && @user.badges.where(rule: 'complete_level_assign?', rule_param: level).empty?
   end
 
 end
